30fa8ef966c0aeb5c2c43418edd0a7e1d1b1a25f
James Park <sanghyeok.park@pepperdine.edu>
2022-10-01 22:21:08 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("myInput").addEventListener("input", myFunction);
+
+function myFunction() {
+    document.getElementById("myInput").value = "asdwdwd";
+}
+
+
+document.getElementById("decimal").addEventListener("input", myFunction2);
+function myFunction2() {
+    document.getElementById("decimal").value = "asdwdwddwdwd";
+}
